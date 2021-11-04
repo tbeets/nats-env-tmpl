f77e31922b6178aa965565595aa9d6a528b8cca1
Todd Beets <todd@beetsme.net>
2021-11-04 11:12:11 -0700

    make more robust by grabbing scripts directory

--- setnscenv.sh
+++ setnscenv.sh
@@ -1,6 +1,8 @@
 # source me from the project root...
 
-PROJ_ROOT=`pwd`
+SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+
+PROJ_ROOT=$SCRIPT_DIR
 
 export NKEYS_PATH=${PROJ_ROOT}/vault/.nkeys
 export NSC_HOME=${PROJ_ROOT}/vault
